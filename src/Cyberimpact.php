60908e7960d3ffec0e8d1365437c996bf3b612a0
Marco Maranao <marco@maranao.ca>
2021-09-03 12:32:46 -0600

    add files

---
+++ src/Cyberimpact.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Cyberimpact;
+
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+
+class Cyberimpact
+{
+  public const API_URL = 'https://api.cyberimpact.com';
+
+  
+}
